815ef6668281fcf2b68e1d96fe6a5f0721f5a393
Bia-Henkel <biadsjh@gmail.com>
2022-09-07 19:18:40 -0300

    Inicio do projeto
    Inicio do projeto.

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+const APP_NAME = 'PHPSTORE';
+const APP_VERSION = '1.0.0';
+
+//mysql
+
+const MYSQL_SERVER = 'localhost';
+const MYSQL_DATABASE = 'php_store';
+const MYSQL_USER = 'root';
+const MYSQL_PASS = '';
+const MYSQL_CHARSET = 'utf8';
---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+//open section
+session_start();
+
+//load config
+require_once('../config.php');
+
+//carrega todas as classes do projeto
+require_once('../vendor/autoload.php');
